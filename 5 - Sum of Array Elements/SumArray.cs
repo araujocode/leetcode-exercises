3397cda09eca11d181626430b4a306f542452a7e
Bruno Araujo <bruno.cadoma@gmail.com>
2024-08-02 20:31:35 -0300

    feat: sum of array elements

---
+++ 5 - Sum of Array Elements/SumArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class SumArray
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Sum of array elements: " + SumArray(numbers)); // Output: 15
+    }
+
+    static int SumArray(int[] array)
+    {
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
