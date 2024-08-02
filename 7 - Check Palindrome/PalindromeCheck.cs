c2764919b1b88fe903b72a51ee7c7e2e6ef70d87
Bruno Araujo <bruno.cadoma@gmail.com>
2024-08-02 20:57:35 -0300

    feat: check palindrome

---
+++ 7 - Check Palindrome/PalindromeCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class PalindromeCheck
+{
+    static void Main()
+    {
+        Console.WriteLine("Check if 'radar' is a palindrome: " + IsPalindrome("radar")); // Output: True
+    }
+
+    static bool IsPalindrome(string s)
+    {
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
