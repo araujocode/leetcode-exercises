bb686c445c049a4e34f1565964602b7151934966
Bruno Araujo <bruno.cadoma@gmail.com>
2024-08-02 20:27:33 -0300

    feat: fibonacci sequence

---
+++ 4 - Fibonacci Sequence/FibonacciSequence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class FibonacciSequence
+{
+    static void Main()
+    {
+        int n = 10;
+        Console.WriteLine("Fibonacci number at position " + n + ": " + Fibonacci(n)); // Output: 55
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+}
