d5c8df116578ae7f0b7fdd815a0251edd00624d3
Bruno Araujo <bruno.cadoma@gmail.com>
2024-08-02 21:00:27 -0300

    feat: bubble sort

---
+++ 8 - Bubble Sort/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class BubbleSort
+{
+    static void Main()
+    {
+        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+        Console.WriteLine("Array before sorting:");
+        PrintArray(array);
+
+        BubbleSort(array);
+
+        Console.WriteLine("Array after sorting:");
+        PrintArray(array);
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
